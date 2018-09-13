bb0e2dd0de2957984d363f93b8d228b6eca7cd9a
sanjay <sanjaynextpage@gmail.com>
2018-09-13 17:30:42 +0530

    second commit

--- src/app/google-map.service.ts
+++ src/app/google-map.service.ts
@@ -1,27 +1,110 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { Observable, BehaviorSubject } from 'rxjs';
-import { Client } from 'elasticsearch';
+
+import { Client } from 'elasticsearch-browser';
+import * as elasticsearch from 'elasticsearch-browser';
 @Injectable({
   providedIn: 'root'
 })
 export class GoogleMapService {
-  private _client: Client;
-  
-  connectionString = 'https://site:ea070f764e629c711854c9e4504c5130@gloin-eu-west-1.searchly.com';
-  client = new this._client.Client({
-              host: this.connectionString,
-              log: 'debug'
-          });
-  _index = "events";
-  _type = 'users';
-  constructor(private http: Http) { }
+  private _client;
 
-  getGeoCode(address): Observable<any>{
+  public connectionString = 'https://site:d81df12619fece0e733b67175bda3698@gloin-eu-west-1.searchly.com';
+
+  _index = 'events';
+  _type = 'users';
+  constructor(private http: Http) {
+   }
+
+   private _connect () {
+     this._client =  new elasticsearch.Client({
+                        host: this.connectionString,
+                        log: 'trace'
+                     });
+
+   }
+
+  getGeoCode(address): Observable<any> {
       const key = 'AIzaSyCXlzLSm5F9D0W16PcX-FQQGi9W51E2SLM';
       let url = `https://maps.googleapis.com/maps/api/geocode/json?key=${key}&address=`;
       url = url + encodeURIComponent(address);
 
       return this.http.get(url);
+  }
+
+  index(data) {
+    this._connect();
+    console.log(data);
+    // this._client.indices.delete({index: this._index});
+    this._client.indices.create({
+      index: this._index,
+      body: {
+      'mappings': {
+        'users': {
+            'properties': {
+                'city': {
+                    'type': 'text',
+                    'fields': {
+                        'raw': {'type': 'keyword'}
+                    }
+                },
+                'country': {
+                    'type': 'text',
+                    'fields': {
+                        'raw': {'type': 'keyword'}
+                    }
+                },
+                'first': {
+                    'type': 'text',
+                    'fields': {
+                        'autocomplete': {'type': 'text', 'analyzer': 'autocomplete'}
+                    }
+                },
+                'last': {
+                    'type': 'text'
+                },
+                'address': {
+                    'type': 'keyword'
+                },
+                'email': {
+                    'type': 'text',
+                    'fields': {
+                        'raw': {'type': 'keyword'}
+                    }
+                },
+                'location': {
+                  'type': 'geo_point'
+                }
+            }
+        }
+      }
+      }
+      , function (error, response) {
+
+          if (error) {
+            throw error;
+          }
+
+          const sampleDataSet = data;
+console.log(sampleDataSet);
+          // const body = [];
+
+          // sampleDataSet.forEach(function (item) {
+          //     body.push({'index': {'_index': this._index, '_type': this._type}});
+          //     body.push(item);
+          // });
+
+          // this._client.index({
+          //     body: body
+          // }, function (err, resp) {
+          //     // res.render('index', {result: 'Indexing Completed!'});
+          //     if (err) {
+          //       console.log(err);
+          //     }
+          //     console.log(resp);
+          // });
+        }
+      });
   }
 }
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -4,36 +4,65 @@ import { AngularFireDatabase } from 'angularfire2/database';
 import { GoogleMapService } from '../google-map.service';
 @Component({
   selector: 'app-main',
-  templateUrl: './main.component.html', 
+  templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
   users = [];
-  geo: any = {}; 
-
-  constructor(public afdb: AngularFireDatabase, private gms: GoogleMapService) { 
+  geo: any = {};
+  elasticUsers = [];
+  constructor(public afdb: AngularFireDatabase, private gms: GoogleMapService) {
     this.afdb.list(`contact`).valueChanges().subscribe(data => {
-      console.log(data)
       data.forEach(user => {
-        if(user.hasOwnProperty('address')){
+        if (user.hasOwnProperty('address')) {
           this.users.push(user);
         }
       });
-    })
+      console.log(this.users);
+    });
   }
 
   ngOnInit() {
-    
+
   }
 
   getGeo(user) {
-    console.log(user.address)
-    this.gms.getGeoCode(user.address).subscribe((data)=>{
-      let response = JSON.parse(data._body);
+    this.gms.getGeoCode(user.address).subscribe((data) => {
+      const response = JSON.parse(data._body);
        this.geo['cord']  = response.results[0].geometry.location;
-       this.geo['email']  = user.email
-       console.log(this.geo)
-    })
+       this.geo['email']  = user.email;
+    });
+  }
+  getGeoCord(user) {
+    this.gms.getGeoCode(user.address).subscribe((data) => {
+      const response = JSON.parse(data._body);
+      const geoCord =   response.results[0].geometry.location;
+         console.log(geoCord);
+
+        //  {
+        //   "name":     "Pala Pizza",
+        //   "location": {
+        //     "lat":     40.722,
+        //     "lon":    -73.989
+        //   }
+        // }
+        const loc =  {
+              'lat':     geoCord.lat,
+              'lon':    geoCord.lng
+            };
+      user['location']  = loc;
+      this.elasticUsers.push(user);
+    });
+  }
+
+  insertGeoCord() {
+    this.users.forEach( user => {
+           this.getGeoCord(user);
+    });
+    // this.gms.index(this.elasticUsers);
+  }
+  createIndex() {
+    this.gms.index(this.elasticUsers);
   }
 
 }
